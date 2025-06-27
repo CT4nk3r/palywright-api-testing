a9faf4797ab6abacd6495f59f5c9636bd6002909
Mate Bosze <Mate.Bosze@quest.com>
2025-06-27 12:28:04 +0200

    create test for the get comments api

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,9 +25,8 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
-
+    /* Base URL to use in actions like `await page.goto('/')`, either get the one from .env or use the one provided in the code */
+    baseURL: process.env.BASE_URL || 'https://jsonplaceholder.typicode.com',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
